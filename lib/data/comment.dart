fd187c347eebff32109f55df91b170101ddf532e
wahab4222 <wahabbabar4222@gmail.com>
2022-11-15 12:16:10 +0500

    added necessary parameters like commentId, productId, and replyId

--- lib/data/comment.dart
+++ lib/data/comment.dart
@@ -5,12 +5,18 @@ class Comment {
   String? avatar;
   String? userName;
   String? content;
+  String commentId;
+  String replyId;
+  String productId;
   DateTime? createAt;
 
   Comment({
     required this.avatar,
     required this.userName,
     required this.content,
+    required this.commentId,
+    required this.productId,
+    required this.replyId,
     this.createAt
   });
 }
